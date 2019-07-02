ece6db22323320c88f4d729b3279aaa5c1eb6d36
ShakshiPandey77 <sakshimr7@gmail.com>
2019-07-02 15:33:35 +0530

    Updates

---
+++ precache-manifest.dda724bf8f32eef79a22eec7a093bcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9797205fa8010ccfaabd8df494edb219",
+    "url": "/Lecture-Portal-Front-end/index.html"
+  },
+  {
+    "revision": "98aacad891bfcd5707b2",
+    "url": "/Lecture-Portal-Front-end/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Lecture-Portal-Front-end/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "98aacad891bfcd5707b2",
+    "url": "/Lecture-Portal-Front-end/static/js/main.5504888f.chunk.js"
+  },
+  {
+    "revision": "82174080ff9af2d3d1e3",
+    "url": "/Lecture-Portal-Front-end/static/js/runtime~main.f2dc931e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lecture-Portal-Front-end/static/media/logo.5d5d9eef.svg"
+  }
+]);
